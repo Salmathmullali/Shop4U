b2e8bb1e66c5797fe8c112877186ca752bd70523
Salmathmullali <135salma7@gmail.com>
2025-03-04 09:24:18 +0000

    fourth commit

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+
+# class CatagoryAdmin(admin.ModelAdmin):
+#     list_display = ('name','image','description')
+admin.site.register(Catagory)
+admin.site.register(Products)
 
 # Register your models here.
