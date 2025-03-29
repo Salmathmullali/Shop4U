4f0e199d8011879f40772336c826204d93ab9ac7
Salmathmullali <135salma7@gmail.com>
2025-03-29 16:42:35 +0000

    fifth_last commit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from . models import *
 
 def home(request):
     return render(request,"index.html")
 def register(request):
     return render(request,"register.html")
+
+def collections(request):
+    catagory=Catagory.objects.filter(status=0)
+    return render(request,"collections.html",{"catagory":Catagory})
