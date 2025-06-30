e5b4dcc01dcaa87cfc86693b0f2a5127822d813b
Salmathmullali <135salma7@gmail.com>
2025-06-30 18:46:58 +0100

    catagory styling

--- app/models.py
+++ app/models.py
@@ -18,7 +18,7 @@ def __str__(self):
     return self.name
 
 class Products(models.Model):
-    caregory=models.ForeignKey(Catagory,on_delete=models.CASCADE)
+    catagory=models.ForeignKey(Catagory,on_delete=models.CASCADE)
     name=models.CharField(max_length=150,null=False,blank=False)
     vendor=models.CharField(max_length=150,null=False,blank=False)
     product_image=models.ImageField(upload_to=getFileName,null=True,blank=True)
