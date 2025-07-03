952b2947caea422064928f9bf06db03bfb6f35ba
Salmathmullali <135salma7@gmail.com>
2025-07-03 18:23:50 +0100

    prodect_catagory

--- app/views.py
+++ app/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
-from . models import *
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 def home(request):
-    return render(request,"index.html")
+    return render(request, "index.html")
+
 def register(request):
-    return render(request,"register.html")
+    return render(request, "register.html")
 
 def collections(request):
-    catagory=Catagory.objects.filter(status=0)
-    return render(request,"collections.html",{"catagory":Catagory})
+    catagory = Catagory.objects.filter(status=0)
+    return render(request, "collections.html", {"catagory": catagory})
+def collection_products(request, cid):
+    catagory = get_object_or_404(Catagory, id=cid, status=0)
+    products = Products.objects.filter(catagory=catagory, status=0)
+
+    context = {
+        "catagory": catagory,
+        "products": products
+    }
+    return render(request, "products_by_catagory.html", context)
